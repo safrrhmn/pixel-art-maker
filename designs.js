d468c32f51222a57f4cbf50fffa68df3ea7cdf59
Saifur Rahman <safrrhmn@hotmail.com>
2018-03-12 08:08:17 -0400

    making some progress

--- designs.js
+++ designs.js
@@ -1,71 +1,34 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-var origColor;
-function makeGrid() {
-  var inputHeight = $("#inputHeight").val();
-  var inputWidth = $("#inputWeight").val();
-  origColor = $("#pixelCanvas").val();
-
-  //just in case if the users create the grid again reset that
-  resetGridToDefault();
-
-  for (let it = 0; it < inputHeight; it++) {
-    $("#pixelCanvas").append(
-      "<tr class='row'>" + "<td class='col'></td>".repeat(inputWidth) + "</tr>"
-    );
+var PixelArtMaker = {};
+PixelArtMaker.Generic = {
+  sortGridHeighWidth: function(sort) {
+    $(sort).sortable();
+    console.debug("${sort} is sorted");
   }
-  toastr.success("A Grid is created successfully!");
-}
+};
 
-$("#sizePicker").on("click", function(e) {
-  e.preventDefault();
-});
-
-function resetGridToDefault() {
-  Array.from($("tr")).forEach(element => {
-    element.remove();
-  });
-}
-
-function resetDom() {
-  var con = confirm(
-    "Are you sure you want to rebuild the grid? All drawings and the canvas will be reset"
-  );
-  if (!con) {
-    return;
+PixelArtMaker.Events = {
+  onClickSubmit: function(onElemnet, element) {
+    $(onElemnet).click(function(evt) {
+      evt.preventDefault();
+      $(element).effect("explode");
+    });
   }
-  var col = $("tr,td");
-  Array.from(col).forEach(element => {
-    $(element).remove();
-  });
-  $("#inputHeight").prop("value", 1);
-  $("#inputWeight").prop("value", 1);
-  toastr.warning("The Grid is reset successfully!");
-}
+};
 
-function resetGrid() {
-  var con = confirm(
-    "Are you sure you want to reset the grid? All drawings will be reset"
-  );
-
-  if (con) {
-    var col = $("#pixelCanvas,tr,td");
-    if (col != "undefined") {
-      Array.from(col).forEach(element => {
-        $(element).removeAttr("style");
-      });
+PixelArtMaker.Grid = {
+  createGrid: function(canvas) {
+    const height = $("inputHeight").val();
+    const width = $("inputWidth").val();
+    for (let i = 0; i < height; i++) {
+      $(canvas).append(
+        "<tr class='row'>" + "<td class='col'></td>".repeat(width) + "</tr>"
+      );
     }
   }
-  toastr.warning("The Grid is cleared successfully!");
-}
+};
 
-$("#pixelCanvas").click(function(e) {
-  var color = $("#colorPicker").val();
-  $(e.target).css("background-color", color);
-});
-
-$("#pixelCanvas").dblclick(function(e) {
-  $(e.target).removeAttr("style");
+$(function() {
+  PixelArtMaker.Generic.sortGridHeighWidth("#sortable");
+  PixelArtMaker.Events.onClickSubmit("#submit", "#sizePicker");
+  PixelArtMaker.Grid.createGrid("#pixelCanvas");
 });
