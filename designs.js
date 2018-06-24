3e094678abb7227d6a445a439c09f0c72ea52d23
Saifur Rahman <safrrhmn@hotmail.com>
2018-06-24 16:11:30 -0400

    modified js file

--- designs.js
+++ designs.js
@@ -1,34 +1,44 @@
 var PixelArtMaker = {};
-PixelArtMaker.Generic = {
-  sortGridHeighWidth: function(sort) {
-    $(sort).sortable();
-    console.debug("${sort} is sorted");
-  }
-};
+const GRID_HEIGHT = "inputHeight";
+const GRID_WIDTH = "inputWidth";
+const SUBMIT = "submit";
+const CANVAS = "pixelCanvas";
+let vCANVAS;
+let vELEM_SUBMIT;
+let vHEIGHT;
+let vWIDTH;
 
 PixelArtMaker.Events = {
-  onClickSubmit: function(onElemnet, element) {
-    $(onElemnet).click(function(evt) {
-      evt.preventDefault();
-      $(element).effect("explode");
-    });
-  }
+  onClickSubmit: function(onElemnet, element) {}
 };
 
 PixelArtMaker.Grid = {
-  createGrid: function(canvas) {
-    const height = $("inputHeight").val();
-    const width = $("inputWidth").val();
-    for (let i = 0; i < height; i++) {
-      $(canvas).append(
-        "<tr class='row'>" + "<td class='col'></td>".repeat(width) + "</tr>"
+  createProperteis: function() {
+    PixelArtMaker.vELEM_SUBMIT = document.getElementById(SUBMIT);
+    PixelArtMaker.vHEIGHT = document.getElementById(GRID_HEIGHT).value;
+    PixelArtMaker.vWIDTH = document.getElementById(GRID_WIDTH).value;
+    PixelArtMaker.vCANVAS = document.getElementById(CANVAS);
+  },
+  createGrid: function() {
+    console.log(
+      `Grid vHEIGHT is ${PixelArtMaker.vHEIGHT} and vWIDTH is ${
+        PixelArtMaker.vWIDTH
+      }`
+    );
+    for (let i = 0; i < PixelArtMaker.vHEIGHT; i++) {
+      PixelArtMaker.vCANVAS.append(
+        "<tr class='row'>" +
+          "<td class='col'></td>".repeat(PixelArtMaker.vWIDTH) +
+          "</tr>"
       );
     }
   }
 };
 
-$(function() {
-  PixelArtMaker.Generic.sortGridHeighWidth("#sortable");
-  PixelArtMaker.Events.onClickSubmit("#submit", "#sizePicker");
-  PixelArtMaker.Grid.createGrid("#pixelCanvas");
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("submit").addEventListener("click", function(ev) {
+    ev.preventDefault();
+    PixelArtMaker.Grid.createProperteis();
+    PixelArtMaker.Grid.createGrid();
+  });
 });
